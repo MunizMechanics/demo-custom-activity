88e601ee39049505805d9f0b7fa4b7e217ca08c3
Maicon Gil <gugamaicon@gmail.com>
2019-06-14 13:52:38 -0300

    did some fixes to make the endpoints work

--- src/main/java/com/example/demo/ActivityController.java
+++ src/main/java/com/example/demo/ActivityController.java
@@ -2,29 +2,34 @@ package com.example.demo;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequestMapping("/activity")
 public class ActivityController {
-    @RequestMapping(value = "/activity/save", method = RequestMethod.POST)
-    public ResponseEntity<String> save () {
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public ResponseEntity<String> save (@RequestBody Map<String, Object> payload) {
         return new ResponseEntity<String>("Save", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/activity/publish", method = RequestMethod.POST)
-    public ResponseEntity<String> publish () {
+    @RequestMapping(value = "/publish", method = RequestMethod.POST)
+    public ResponseEntity<String> publish (@RequestBody Map<String, Object> payload) {
         return new ResponseEntity<String>("Publish", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/activity/validate", method = RequestMethod.POST)
-    public ResponseEntity<String> validate () {
+    @RequestMapping(value = "/validate", method = RequestMethod.POST)
+    public ResponseEntity<String> validate (@RequestBody Map<String, Object> payload) {
         return new ResponseEntity<String>("Validate", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/activity/execute", method = RequestMethod.POST)
-    public ActivityResult execute () {
+    @RequestMapping(value = "/execute", method = RequestMethod.POST)
+    public ActivityResult execute (@RequestBody Map<String, Object> payload) {
         return new ActivityResult("true");
     }
 }
